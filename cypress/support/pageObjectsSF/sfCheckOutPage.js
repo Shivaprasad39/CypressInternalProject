44fc9816aa9ce887905a47ef06725698f2161ee1
ShivaIndium <shivaprasad.r@indiumsoft.com>
2023-01-10 23:11:21 +0530

    Added HDollar page objects

--- cypress/support/pageObjectsSF/sfCheckOutPage.js
+++ cypress/support/pageObjectsSF/sfCheckOutPage.js
@@ -10,8 +10,26 @@ class sfCheckOutPage
     getPayByChequeBtn(){
         return cy.contains('Pay by cheque')
     }
-    getPlaceOrderBtn(){
+    getChequePlaceOrderBtn(){
         return cy.contains('Place order')
+    }
+    getPayByHDollarBtn(){
+        return cy.contains("PAY WITH H DOLLAR")
+    }
+    getFirstNameField(){
+        return cy.get('#firstName')
+    }
+    getLastNameField(){
+        return cy.get('#lastName')
+    }
+    getHimalayaEmailField(){
+        return cy.get('#helogin')
+    }
+    getHDollarCheckBox(){
+        return cy.get('input[name="agreement"]')
+    }
+    getHDollarPlaceOrderBtn(){
+        return cy.get('button[type="submit"]:nth-child(1)')
     }
     getOrderNumber(){
         return cy.get("a[href='/en/account']~p:nth-child(5)")
